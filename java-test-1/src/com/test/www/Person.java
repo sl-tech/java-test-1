dce55675e422a7a66817f09be849da5c46973f1b
Sammy Lo <sammy.lo@coraltreesystems.com>
2016-04-10 09:21:31 +0100

    First commit

---
+++ java-test-1/src/com/test/www/Person.java
@@ -0,0 +1,15 @@
+package com.test.www;
+
+public class Person {
+    // fields
+	private String name;  //name of dude
+	private int maxBooks; //max books dude can rent
+	
+	// contructors (parms)
+	public Person() {
+		name = "lo-rider";
+		maxBooks = 20;
+	}
+	
+	
+}
