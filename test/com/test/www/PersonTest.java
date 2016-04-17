e0ca4aa6d1d8256f1606c46924631b133910edcc
Sammy Lo <sammy.lo@coraltreesystems.com>
2016-04-17 14:56:22 +0100

    just wanted to commit something that works

---
+++ test/com/test/www/PersonTest.java
@@ -0,0 +1,24 @@
+package com.test.www;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+	@Test
+	public void testPerson() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMaxBooks() {
+		fail("Not yet implemented");
+	}
+
+}
